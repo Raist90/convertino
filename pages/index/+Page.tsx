caab8aa8eb0f41210e5d0cfb16ee7087bda38347
Jaromil90 <manuel.detoma@noaignite.com>
2024-06-13 20:32:43 +0200

    add Modal component

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,9 +1,24 @@
+import { Modal } from '@/components/Modal'
+import { useDialog } from '@/helpers/useDialog'
 import React from 'react'
 
 export default function Page() {
+  const { closeDialog, dialogRef, handleClick, isOpen } = useDialog()
+
   return (
     <>
-      <h1>Home</h1>
+      <button onClick={handleClick} className='border p-4'>
+        Home
+      </button>
+      <Modal
+        closeDialog={closeDialog}
+        isOpen={isOpen}
+        ref={dialogRef}
+        title={'Dialog title'}
+        renderTitle={true}
+      >
+        <button>Hello</button>
+      </Modal>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import vercel from "vite-plugin-vercel";
-import ssr from "vike/plugin";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
+import ssr from 'vike/plugin'
+import { defineConfig } from 'vite'
+import vercel from 'vite-plugin-vercel'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react({}), ssr({
-    prerender: true,
-  }), vercel()],
-});
+  plugins: [
+    tsconfigPaths(),
+    react({}),
+    ssr({
+      prerender: true,
+    }),
+    vercel(),
+  ],
+})
