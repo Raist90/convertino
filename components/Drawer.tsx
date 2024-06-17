e8195b9dde42e396cc5abc72d69f938a3579eb38
Jaromil90 <manuel.detoma@noaignite.com>
2024-06-17 13:34:03 +0200

    tweak Drawer

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -1,5 +1,3 @@
-import clsx from 'clsx'
-
 export { Drawer }
 
 type DrawerProps = {
@@ -9,13 +7,12 @@ type DrawerProps = {
 
 function Drawer({ children, isOpen }: DrawerProps) {
   return (
-    <div
-      className={clsx(
-        isOpen ? 'block' : 'hidden',
-        'w-full border-b border-black p-4 pt-0',
+    <>
+      {isOpen && (
+        <div className='w-full border-b border-black p-4 pt-0'>
+          <div>{children}</div>
+        </div>
       )}
-    >
-      <div>{children}</div>
-    </div>
+    </>
   )
 }
